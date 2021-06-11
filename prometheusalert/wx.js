1c8437992034ebb9c1b929880d70b7f590580769
GGBord <56374914+LzxDarren@users.noreply.github.com>
2021-06-11 18:29:21 +0800

    Update wx.js

--- prometheusalert/wx.js
+++ prometheusalert/wx.js
@@ -12,7 +12,7 @@ exports.template = function(body) {
                 return ["------------------------------"]
                 .concat("##            告警来了")
                 .concat("------------------------------")
-                .concat(`### 告警名称:${alert.labels.alertname}`)
+                .concat(`### 告警名称:${alert.labels.alertname} ${noticeMem}`)
                 .concat(`### 状态:<font color="${body.status === 'firing' ? 'warning' : 'info'}">${body.status}</font>`)
                 .concat(`### 告警实例:${alert.labels.instance}`)
                 .concat(`### 告警等级:${alert.labels.severity}`)
